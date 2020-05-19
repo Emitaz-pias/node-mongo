a933361fc4802b7508d8eeeb4fc22e569c2b5354
Emitaz-pias <61836021+Emitaz-pias@users.noreply.github.com>
2020-05-19 23:50:05 +0600

    react added via node

--- index.js
+++ index.js
@@ -12,18 +12,52 @@ app.use(bodyParser.json());
 
 const uri = process.env.DB_PATH;
 let client = new MongoClient(uri, {useUnifiedTopology:true , useNewUrlParser: true});
-// const users = ["pias" , "bappi" ,"monni" ,"shikhon" ,"liza"]
-// app.get('/user/:id' ,(req ,res) =>{
-//     const id =req.params.id
-//     const name = users[id]
-//     res.send({ id,name})
-// })
+const users = ["pias" , "bappi" ,"monni" ,"shikhon" ,"liza"]
+//load a single product to product details
+app.get('/product/:key' ,(req ,res) =>{
+    const key =req.params.key
+client = new MongoClient(uri, {useUnifiedTopology:true , useNewUrlParser: true});
+client.connect(err =>{
+    const collection = client.db("onlineStore").collection("products");
+     collection.find({key}).toArray((err,documents ) =>{   
+        if (err) {
+            console.log(err);
+           res.status(500).send({message:err}) 
+        } else {
+         res.send(documents[0])  
+        }
+     });
+    
+   })
+   client.close();
 
+})
+//lodaing multiple product to order review
+app.post('/getProductKey' ,(req ,res) =>{
+const key =req.params.key;
+const productKeys =  req.body;
+client = new MongoClient(uri, {useUnifiedTopology:true , useNewUrlParser: true});
+client.connect(err =>{
+   const collection = client.db("onlineStore").collection("products");
+    collection.find({key: {$in:productKeys}}).toArray((err,documents ) =>{   
+       if (err) {
+           console.log(err);
+          res.status(500).send({message:err}) 
+       } else {
+        res.send(documents)  
+       }
+    });
+   
+  })
+  client.close();
+
+})
+//lodaing all product to shop
 app.get('/Products', (req, res) =>{
 client = new MongoClient(uri, {useUnifiedTopology:true , useNewUrlParser: true});
 client.connect(err =>{
     const collection = client.db("onlineStore").collection("products");
-     collection.find({price:{$eq:'900'}}).limit(100) .toArray((err,documents ) =>{   
+     collection.find().toArray((err,documents ) =>{   
         if (err) {
             console.log(err);
            res.status(500).send({message:err}) 
@@ -34,21 +68,39 @@ client.connect(err =>{
     
    })
    client.close();
+
 });
 
-// const atlasfor this project  username : pias and pass : aG7MWuubmyjqQAx
+
+app.post('/placeOrder',(req ,res ) =>{
+   client = new MongoClient(uri, {useUnifiedTopology:true , useNewUrlParser: true});
+   const orderDetails =  req.body;
+   orderDetails.orderTime = new Date(); 
+   client.connect(err =>{
+      const collection = client.db("onlineStore").collection("orders");
+       collection.insertOne(orderDetails , (err,result ) =>{   
+          if (err) {
+              console.log(err);
+             res.status(500).send({message:err}) 
+          } else {
+           res.send(result.ops[0])  
+          }
+       });
+      
+     });
+     client.close();
+      
+});
 
 
-
-//post 
-
-app.post('/addUser' ,(req ,res ) =>{
+//post  allproduct to database
+app.post('/addProduct' ,(req ,res ) =>{
     client = new MongoClient(uri, {useUnifiedTopology:true , useNewUrlParser: true});
     const product =  req.body;
 
     client.connect(err =>{
        const collection = client.db("onlineStore").collection("products");
-        collection.insertOne(product , (err,result ) =>{   
+        collection.insert(product , (err,result ) =>{   
            if (err) {
                console.log(err);
               res.status(500).send({message:err}) 
@@ -61,5 +113,5 @@ app.post('/addUser' ,(req ,res ) =>{
       client.close();
        
 });
-
+ process.env.DB_PORT
 app.listen(process.env.DB_PORT, () => console.log(`pias i'm listening`));
